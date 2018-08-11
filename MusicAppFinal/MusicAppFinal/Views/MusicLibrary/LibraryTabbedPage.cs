c35b2ec6f32dbfb2cc7bf5bf863ef6f6048c08c0
NokukhanyaD <NokukhanyaD@ZAZN-KOS-DIT002>
2018-08-11 13:49:15 +0200

    fix : build

---
+++ MusicAppFinal/MusicAppFinal/Views/MusicLibrary/LibraryTabbedPage.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace MusicAppFinal.Views.MusicLibrary
+{
+    public class LibraryTabbedPage : TabbedPage
+    {
+        public LibraryTabbedPage()
+        {
+            Children.Add(new PlaylistPage()
+            {
+                Title = "PLAYLISTS",
+                Icon = Device.OnPlatform<string>("tab_feed.png", null, null)
+            });
+            Children.Add(new ArtistPage()
+            {
+                Title = "ARTISTS",
+                Icon = Device.OnPlatform<string>("tab_about.png", null, null)
+            });
+            Children.Add(new AlbumPage()
+            {
+                Title = "ALBUMS",
+                Icon = Device.OnPlatform<string>("tab_about.png", null, null)
+
+            });
+            Children.Add(new SongPage()
+            {
+                Title = "SONGS",
+                Icon = Device.OnPlatform<string>("tab_about.png", null, null)
+
+            });
+
+
+
+        }
+    }
+}
